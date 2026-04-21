50dc66db397cfd8df8c422f8f41481647bba4c1d
Srihari Kodati <90450520+srihari-kodati@users.noreply.github.com>
2026-04-21 18:42:42 +0530

    Lion

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f dragon "Run for cover, I'm a DRAGON ... RAWR" >> dragon.txt
-grep -i "dragon" dragon.txt
-cat dragon.txt
+cowsay -f Lion "Run for cover, I'm a Lion ... RAWR" >> lion.txt
+grep -i "dragon" lion.txt
+cat lion.txt
 ls -ltra
